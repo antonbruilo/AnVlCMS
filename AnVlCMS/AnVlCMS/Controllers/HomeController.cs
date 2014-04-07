ce112e4db883fe2475c14fe3c37da5c2f6a61a2a
Антон Бруйло <antonbruilo@gmail.com>
2014-04-07 21:09:24 +0300

    123
    123

--- AnVlCMS/AnVlCMS/Controllers/HomeController.cs
+++ AnVlCMS/AnVlCMS/Controllers/HomeController.cs
@@ -13,10 +13,13 @@ namespace AnVlCMS.Controllers
 
         public ActionResult Index()
         {
+<<<<<<< HEAD
             int i = 10;
             int j = 100;
 
 
+=======
+>>>>>>> 5c844b493722369d663e74a9bef9ff096ad8b678
             return View();
         }
 
