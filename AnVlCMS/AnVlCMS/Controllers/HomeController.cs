ee3fccc7c05b72deaed55c7955e42c1228b3b56d
Wladdek <Wladdek@mail.ru>
2014-04-07 21:07:41 +0300

    описание
    полное описание

--- AnVlCMS/AnVlCMS/Controllers/HomeController.cs
+++ AnVlCMS/AnVlCMS/Controllers/HomeController.cs
@@ -13,11 +13,10 @@ namespace AnVlCMS.Controllers
 
         public ActionResult Index()
         {
-<<<<<<< HEAD
             int i = 10;
+            int j = 100;
 
-=======
->>>>>>> 5c844b493722369d663e74a9bef9ff096ad8b678
+
             return View();
         }
 
